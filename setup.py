ce5233749128f2ef3c65103e1f842c5d40943543
Nicholas Ramkissoon <nr1610@nyu.edu>
2019-07-09 16:52:05 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="colorstats",
-    version="1.1.2",
+    version="1.2.0",
     author="Nicholas Ramkissoon",
     description="Package for processing color data from images",
     long_description=long_description,
